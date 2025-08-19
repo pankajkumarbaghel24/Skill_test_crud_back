d0125bdc63cc114984d9a5858a3723b549c32809
pankajkumarbaghel24 <pankajbaghel88880888@gmail.com>
2025-08-19 11:29:28 +0530

    first commit

---
+++ mongodb.js
@@ -0,0 +1,27 @@
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
+
+const url = `mongodb+srv://root:root@uniconnectnms.fadvayz.mongodb.net/?retryWrites=true&w=majority&appName=UniConnectNMS`;
+
+let _db;
+const mongoConnect = (callback)=>{
+    MongoClient.connect(url)
+    .then((client)=>{
+
+        callback();
+        _db = client.db('skilltest');
+    })
+    .catch((error)=>{
+        console.log("MongoDB Connection Error:-",error)
+    });
+};
+
+const getDB = () =>{
+    if(!_db){
+        throw new Error('Mongo not connected');
+    }
+    return _db;
+}
+
+exports.mongoConnect=mongoConnect;
+exports.getDB=getDB;
